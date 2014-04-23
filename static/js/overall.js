61c1f25d85a9ced565657275950031537bec9388
Efe Karakus <efekarakus@gmail.com>
2014-04-23 18:04:43 -0500

    legend added

--- static/js/overall.js
+++ static/js/overall.js
@@ -21,8 +21,6 @@ function drawAllPlayers( selector )
     function _getCoords( meta, player )
     {
 
-        console.log( player );
-
         var sxCone = d3.scale.linear()
             .domain( [ meta['3cone'][0], meta['3cone'][2] ] )
             .range( [35.0, 0.0] );
@@ -78,6 +76,151 @@ function drawAllPlayers( selector )
         return coords;
     }
 
+    function _drawExample(selector, meta)
+    {
+        var exampleDiv = d3.select(selector)
+            .append('div')
+            .attr('class', 'example');
+
+        var svg = exampleDiv.append('svg')
+            .attr("width", 140)
+            .attr("height", 70);
+
+        svg.selectAll(".inner")
+            .data( [_getCoords( meta, { 
+                    "3cone": meta["3cone"][1],
+                    "40yd": meta["40yd"][1],
+                    "benchpress": meta["benchpress"][1],
+                    "broadjump": meta["broadjump"][1],
+                    "vertleap": meta["vertleap"][1],
+                    "shuttle": meta["shuttle"][1]
+                }
+            )] )
+            .enter()
+                .append("polygon")
+                .attr("class", "inner")
+                .attr("points", function(d) {
+                    return d.map(function(d) { return [d.x, d.y].join(","); }).join(" ");
+                });
+
+        svg.selectAll(".outer")
+            .data([[ 
+                {"x": 20, "y": 20},
+                {"x": 50, "y": 20},
+                {"x": 55, "y": 35},
+                {"x": 50, "y": 50},
+                {"x": 20, "y": 50},
+                {"x": 15, "y": 35}
+            ]]).enter()
+                .append("polygon")
+                .attr("class", "outer")
+                .attr("points", function(d) {
+                    return d.map(function(d) { return [d.x, d.y].join(","); }).join(" ");
+                });
+
+        /**
+        svg.append("text")
+            .attr("x", 0)
+            .attr("y", 22)
+            .text("3-cone")
+            .attr("transform", "rotate(-20)");
+
+        svg.append("text")
+            .attr("x", 42)
+            .attr("y", -3)
+            .text("40 yard")
+            .attr("transform", "rotate(20)");
+
+        svg.append("text")
+            .attr("x", 55)
+            .attr("y", 35)
+            .text("bench press");
+        */
+
+        svg.append("rect")
+            .attr("x", "110")
+            .attr("y", "20")
+            .attr("width", 20)
+            .attr("height", 10)
+            .style("fill", "#505050")
+            .style("opacity", "0.5")
+            .style("stroke", "#000")
+            .style("stroke-width", "0.5");
+
+        svg.append("rect")
+            .attr("x", 110)
+            .attr("y", 35)
+            .attr("width", 20)
+            .attr("height", 10)
+            .style("fill", "#202020")
+            .style("opacity", "0.8")
+            .style("stroke", "#000")
+            .style("stroke-width", "0.5");
+
+        svg.append("text")
+            .attr("x", 70)
+            .attr("y", 28)
+            .text("Average")
+            .style("color", "#505050")
+            .style("opacity", "0.5");
+
+        svg.append("text")
+            .attr("x", 75)
+            .attr("y", 42)
+            .text("Player")
+            .style("color", "#202020")
+            .style("opacity", "0.8");
+
+
+        exampleDiv.append('div')
+            .attr('class', 'name')
+            .text('Example')
+    }
+
+    function _drawPlayers(selector, players, meta)
+    {
+        var playerDivs = d3.select(selector)
+            .selectAll('player')
+            .data(players)
+            .enter()
+                .append('div')
+                .attr('class', 'player');
+
+        var svg = playerDivs.append('svg')
+            .attr("width", 70)
+            .attr("height", 70);
+
+        svg.selectAll(".inner")
+            .data( [_getCoords( meta, { 
+                    "3cone": meta["3cone"][1],
+                    "40yd": meta["40yd"][1],
+                    "benchpress": meta["benchpress"][1],
+                    "broadjump": meta["broadjump"][1],
+                    "vertleap": meta["vertleap"][1],
+                    "shuttle": meta["shuttle"][1]
+                }
+            )] )
+            .enter()
+                .append("polygon")
+                .attr("class", "inner")
+                .attr("points", function(d) {
+                    return d.map(function(d) { return [d.x, d.y].join(","); }).join(" ");
+                });
+
+        svg.selectAll(".outer")
+            .data( function(player) { return [_getCoords( meta, player )]; } )
+            .enter()
+                .append("polygon")
+                .attr("class", "outer")
+                .attr("points", function(d) {
+                    return d.map(function(d) { return [d.x, d.y].join(","); }).join(" ");
+                });
+
+        var name = playerDivs.append('div')
+            .attr('class', 'name')
+            .text(function( d ) { return d.name.text; } );
+    }
+
     d3.json( "../data/pruned-nfl-combine-2013.json", 
         function( err, json ) 
         {
@@ -85,51 +228,16 @@ function drawAllPlayers( selector )
 
             var meta = json[ "meta" ];
 
+            // clean data
             var players = json[ 'players' ];
             players.forEach( function( player ) 
             {
                 _clean( player );
             } );
 
-            var playerDivs = d3.select(selector)
-                .selectAll('player')
-                .data(players)
-                .enter()
-                    .append('div')
-                    .attr('class', 'player');
+            _drawExample(selector, meta);
 
-            var svg = playerDivs.append('svg')
-                .attr("width", 70)
-                .attr("height", 70);
-
-            svg.selectAll(".inner")
-                .data( [_getCoords( meta, { 
-                        "3cone": meta["3cone"][1],
-                        "40yd": meta["40yd"][1],
-                        "benchpress": meta["benchpress"][1],
-                        "broadjump": meta["broadjump"][1],
-                        "vertleap": meta["vertleap"][1],
-                        "shuttle": meta["shuttle"][1]
-                    }
-                )] )
-                .enter()
-                    .append("polygon")
-                    .attr("class", "inner")
-                    .attr("points", function(d) {
-                        return d.map(function(d) { return [d.x, d.y].join(","); }).join(" ");
-                    });
-
-            svg.selectAll(".outer")
-                .data( function(player) { return [_getCoords( meta, player )]; } )
-                .enter()
-                    .append("polygon")
-                    .attr("class", "outer")
-                    .attr("points", function(d) {
-                        return d.map(function(d) { return [d.x, d.y].join(","); }).join(" ");
-                    });
-
-            var name = playerDivs.append('div')
-                .attr('class', 'name')
-                .text(function( d ) { return d.name.text; } );
+            _drawPlayers(selector, players, meta);
+            
         });
 }
