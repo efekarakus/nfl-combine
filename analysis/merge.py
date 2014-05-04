4b009c76f8973a1778a90d7e59c7d3f97516fbcd
Efe Karakus <efekarakus@gmail.com>
2014-05-04 02:27:15 -0500

    adding 2014 data and merged certain positions together

--- analysis/merge.py
+++ analysis/merge.py
@@ -5,37 +5,67 @@ features = ['name', 'pos', 'height', 'weight',
 	'broadjump', 'shuttle', '3cone']
 
 
+positions = {
+    # offense
+    #########
+    # merge as kickers
+    'P': 'K',
+    'K': 'K',
+
+    # merge into offensive line
+    'OT': 'OL',
+    'OG': 'OL',
+    'LS': 'OL',
+    'C': 'OL',
+
+    # defense
+    #########
+
+    # safeties
+    'SS': 'S',
+    'FS': 'S',
+
+    # line backers
+    'OLB': 'LB',
+    'ILB': 'LB',
+
+    # defensive line
+    'DF': 'DL',
+    'DE': 'DL'
+}
+
 def merge(paths):
+    def isValid(player):
+        pos = player['pos']
+        if pos == 'QB' and player['benchpress'] == '':
+            player['benchpress'] = '0.0'
 
-	def isValid(player):
-		pos = player['pos']
-		if pos == 'QB' and player['benchpress'] == '':
-			player['benchpress'] = '0.0'
+        for feature in features[5:]:
+            if player[feature] == '':
+                return False
+        return True
 
-		for feature in features[5:]:
-			if player[feature] == '':
-				return False
-		return True
+    def clean(player):
+        if player['pos'] in positions:
+            player['pos'] = positions[player['pos']]
 
-	def clean(player):
-		for feature in features[2:]:
-			if player[feature] != '':
-				val = player[feature]
-				if val[0] == '*':
-					val = val[1:]
-				player[feature] = float(val)
+        for feature in features[2:]:
+            if player[feature] != '':
+                val = player[feature]
+                if val[0] == '*':
+                    val = val[1:]
+                player[feature] = float(val)
 
-	data = []
-	for path in paths:
-		j = json.load(open(path, 'r'))
-		players = j['players']
+    data = []
+    for path in paths:
+        j = json.load(open(path, 'r'))
+        players = j['players']
 
-		for player in players:
-			if isValid(player):
-				clean(player)
-				data.append(player)
-
-	return data
+        for player in players:
+            if isValid(player):
+                clean(player)
+                data.append(player)
+    return data
 
 def write_csv(path , players):
 
@@ -56,6 +86,7 @@ def write_csv(path , players):
 
 if __name__ == '__main__':
 	paths = [
+        '../data/nfl-combine-2014.json',
 		'../data/nfl-combine-2013.json',
 		'../data/nfl-combine-2012.json',
 		'../data/nfl-combine-2011.json'
