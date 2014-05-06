b69ebde93b274e0efea7c1da416ecbb5679cfdd2
Efe Karakus <efekarakus@gmail.com>
2014-05-05 21:39:43 -0500

    find better positions for outliers

--- analysis/outliers/outliers.py
+++ analysis/outliers/outliers.py
@@ -84,7 +84,117 @@ def plot_scans(scans):
         count += 1
     plt.show()
 
+def find_otherpositions(scans):
+    def read_eigenvectors():
+        eigens = {}
+        with open('../../data/eigen_vectors.csv', 'r') as f:
+            next(f)
+            reader = csv.reader(f)
+            for row in reader:
+                pos = row[0]
+                eigen = [float(val) for val in row[1:]]
+
+                if not pos in eigens:
+                    eigens[pos] = []
+
+                eigens[pos].append( eigen )
+        return eigens
+
+    def get_name(pos, x, y):
+        with open('../../data/transformed_all.csv', 'r') as f:
+            next(f)
+            reader = csv.reader(f)
+            for row in reader:
+                ppos = row[0]
+                pname = row[1]
+                px = float(row[2])
+                py = float(row[3])
+
+                if pos == ppos:
+                    if x == px and y == py:
+                        return pname
+        return None
+
+    def get_features(pos, name):
+        with open('../../data/all.csv', 'r') as f:
+            next(f)
+            reader = csv.reader(f)
+            for row in reader:
+                pname = row[0]
+                ppos = row[1]
+
+                features = [float(val) for val in row[5:]]
+                if pname == name and ppos == pos:
+                    return features
+        return None
+
+    def get_feature_means():
+        # TODO update means to be based on positions
+        means = {}
+        n = {}
+        with open('../../data/all.csv', 'r') as f:
+            next(f)
+            reader = csv.reader(f)
+            for row in reader:
+                pos = row[1]
+                if not pos in means:
+                    means[pos] = [0,0,0,0,0,0]
+                    n[pos] = 0
+
+                features = [float(val) for val in row[5:]]
+                for i in range(6):
+                    means[pos][i] += features[i]
+
+                n[pos] += 1
+
+        for pos in means:
+            for i in range(6):
+                means[pos][i] = float(means[pos][i])/n[pos]
+        return means
+
+
+    eigens = read_eigenvectors()
+    means = get_feature_means()
+
+    for pos in scans:
+        scan = scans[pos]
+        outliers = scan.get_outliers()
+
+        for outlier in outliers:
+            x = outlier[0]
+            y = outlier[1]
+
+            if x >= 0: continue
+
+            # ONLY CONSIDER BAD PLAYERS
+
+            name = get_name(pos, x, y)
+            features = get_features(pos, name)
+
+            print 'original: ', name, pos, str([x,y])
+            for opos in eigens:
+                if pos == opos: continue
+
+                xEigen = eigens[opos][0]
+                yEigen = eigens[opos][1]
+
+                scaled_features = [0,0,0,0,0,0]
+
+                for i in range(6):
+                    scaled_features[i] = features[i] - means[opos][i]
+
+                newX = sum(p*q for p,q in zip(scaled_features, xEigen))
+                newY = sum(p*q for p,q in zip(scaled_features, yEigen))
+
+                if newX >= 0 and newY >= 0:
+                    print 'other: ', name, opos, str([newX,newY])
+
+        # endfor outlier
+    # endfor eigens
+
 if __name__ == '__main__':
     points = read_points('../../data/transformed_all.csv')
     scans = scan_points(points)
-    plot_scans(scans)
+    
+    find_otherpositions(scans)
+    #plot_scans(scans)
