13082f9d827c4388a41e2682f2a9443542dddeaf
Paul Miller <paul@paulmillr.com>
2014-05-15 20:41:09 -0500

    adding meta data at the top of the file + ids

--- analysis/split.py
+++ analysis/split.py
@@ -15,10 +15,13 @@ def read_data():
     
 def write_positions(data):
     for pos in data:
+        pid = 1
         with open('../data/transformed-' + pos + '.csv', 'w') as f:
+            f.write('id,pos,name,x,y\n')
             for row in data[pos]:
                 s = ','.join(row)
-                f.write(s + '\n')
+                f.write(str(pid) + ',' + s + '\n')
+                pid += 1
 
 if __name__ == '__main__':
     data = read_data()
